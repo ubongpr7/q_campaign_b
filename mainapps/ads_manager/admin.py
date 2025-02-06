5354d85ac25ceda0f48f79a8fe6fd53a7db7ad61
UbongPr7 <ubongpr7@gmail.com>
2025-02-06 11:22:08 +0000

    ad manager admin

--- mainapps/ads_manager/admin.py
+++ mainapps/ads_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ad,AdSet,FaceBookAdAccount,Campaign
 
 # Register your models here.
+admin.site.register(Ad)
+admin.site.register(AdSet)
+admin.site.register(FaceBookAdAccount)
+admin.site.register(Campaign)
